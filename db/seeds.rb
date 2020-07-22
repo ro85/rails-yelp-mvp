9f7167513107853b22ed224148e4e912eba43a4a
Rodrigo Villanueva <r_villanueva18@hotmail.com>
2020-07-22 16:18:02 -0300

    rails-yelp-mvp. Solve. pending css

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,5 @@
+require 'faker'
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
@@ -5,3 +7,20 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts "Cleaning database..."
+Restaurant.destroy_all
+
+
+puts "Creating restaurants..."
+10.times do
+  restaurant = Restaurant.new(
+    name: Faker::Name.name, 
+    address: Faker::Address.street_address,
+    phone_number: Faker::PhoneNumber.cell_phone,
+    category: Restaurant::CATEGORIES.sample
+  )
+  restaurant.save!
+end
+
+puts "Finished!"
